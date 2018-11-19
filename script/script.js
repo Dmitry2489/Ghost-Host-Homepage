051c8fe3ca9e7914fceee3384c6abf88614bbdc0
unknown <iva.klicunov2489@mail.ru>
2018-11-19 14:48:33 +0300

    one commit

---
+++ script/script.js
@@ -0,0 +1,25 @@
+$(window).scroll(function() {
+
+    var st = $(this).scrollTop();
+    $("h1, .header-text").css({
+        "transform" : "translate(0%, " + st /3 + "0%"
+    });
+});
+
+$(document).ready(function(){
+	
+	// Находим блок карусели
+	var carousel = $("#carousel");
+
+	// Запускаем плагин карусели
+	carousel.owlCarousel({
+        // Точки под каруселью
+        itemsDesktop: [1600, 1],
+        itemsTablet: [768,1],
+        loop: true,
+        autoPlay: true,
+        autoPlayTimeout: 500,
+        items: 1,
+        scrollPerPage: 1,
+    });
+});
